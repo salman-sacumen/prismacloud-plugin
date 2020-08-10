ba4f3a0c60ffd9f757ba071cefbbebd5d3d7f1f1
salman-sacumen <muhammed.salman@sacumen.com>
2020-08-11 03:26:50 +0530

    aa

--- src/main/java/com/atlassian/bitbucket/server/hook/PrismaMergeRepositoryHook.java
+++ src/main/java/com/atlassian/bitbucket/server/hook/PrismaMergeRepositoryHook.java
@@ -8,10 +8,15 @@ import com.atlassian.bitbucket.setting.*;
 
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PrismaMergeRepositoryHook implements RepositoryMergeRequestCheck, RepositorySettingsValidator
 {
     private static final Pattern NUMBER_PATTERN = Pattern.compile("[0-9]+");
-
+    private static final Logger log = LoggerFactory.getLogger("PrismaMergeRepositoryHook.class");
+    
+    
     /**
      * Vetos a pull-request if there aren't enough reviewers.
      */
@@ -19,6 +24,9 @@ public class PrismaMergeRepositoryHook implements RepositoryMergeRequestCheck, R
     public void check(RepositoryMergeRequestCheckContext context)
     {
         int requiredReviewers = Integer.parseInt(context.getSettings().getString("reviewers"));
+        
+    	log.info("high count "+context.getSettings().getString("allowedhighcount"));
+    	log.info("reviewers "+requiredReviewers);
         int acceptedCount = 0;
         for (PullRequestParticipant reviewer : context.getMergeRequest().getPullRequest().getReviewers())
         {
@@ -33,6 +41,7 @@ public class PrismaMergeRepositoryHook implements RepositoryMergeRequestCheck, R
     @Override
     public void validate(Settings settings, SettingsValidationErrors errors, Repository repository)
     {
+        log.info("high count value  "+settings.getString("allowedhighcount"));
         String numReviewersString = settings.getString("reviewers", "0").trim();
         if (!NUMBER_PATTERN.matcher(numReviewersString).matches())
         {
